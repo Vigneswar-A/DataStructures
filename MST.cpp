8304f7e7d9f4916cb47e65f49688d350fa511fd9
Vigneswar-A <ugiv.rawsengiv@gmail.com>
2022-11-24 23:01:33 +0530

    MST added

--- MST.cpp
+++ MST.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
-#include <stack>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+class Edge
+{
+public:
+    int src;
+    int dest;
+    int weight;
+    Edge(int src, int dest, int weight)
+        : src(src), dest(dest), weight(weight) {}
+};
+
+bool operator<(const Edge &edge1, const Edge &edge2)
+{
+    return edge1.weight > edge2.weight;
+}
+
 class Graph
 {
-    int **adjacency;
+    int ***adjacency;
     int n;
     int *visited;
 
@@ -28,7 +41,7 @@ private:
             cout << node << " ";
 
             for (int i = 0; i < n; i++)
-                if (adjacency[node][i] && !visited[i])
+                if (adjacency[node][i][0] && !visited[i])
                 {
                     stack_.push(i);
                     visited[i] = 1;
@@ -54,7 +67,7 @@ private:
             cout << node << " ";
 
             for (int i = 0; i < n; i++)
-                if (adjacency[node][i] && !visited[i])
+                if (adjacency[node][i][0] && !visited[i])
                 {
                     queue_.push(i);
                     visited[i] = 1;
@@ -71,13 +84,17 @@ public:
         */
 
         n = nodes;
-        adjacency = new int *[n];
+        adjacency = new int **[n];
 
         for (int i = 0; i < n; i++)
         {
-            adjacency[i] = new int[n];
+            adjacency[i] = new int *[n];
             for (int j = 0; j < n; j++)
-                adjacency[i][j] = 0;
+            {
+                adjacency[i][j] = new int[2];
+                adjacency[i][j][0] = 0;
+                adjacency[i][j][1] = 0;
+            }
         }
 
         visited = new int[n];
@@ -93,12 +110,12 @@ public:
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
-                cout << adjacency[i][j] << " ";
+                cout << "[" << adjacency[i][j][0] << ", " << adjacency[i][j][1] << "] ";
             cout << endl;
         }
     }
 
-    bool add_edge(int u, int v, int dir = 0)
+    bool add_edge(int u, int v, int weight = 0, int dir = 0)
     {
         /*
             add edge between vertices u and v, dir = 1 implies directed edge.
@@ -106,9 +123,13 @@ public:
 
         if (u >= n || v >= n)
             return false;
-        adjacency[u][v] = 1;
+        adjacency[u][v][0] = 1;
+        adjacency[u][v][1] = weight;
         if (dir == 0)
-            adjacency[v][u] = 1;
+        {
+            adjacency[v][u][0] = 1;
+            adjacency[v][u][1] = weight;
+        }
         return true;
     }
 
@@ -157,7 +178,7 @@ public:
             simply removes any edge between vertices u and v if exists.
         */
         if (u < n && v < n)
-            adjacency[u][v] = 0;
+            adjacency[u][v][0] = 0;
     }
 
     void clear_graph()
@@ -168,7 +189,43 @@ public:
 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                adjacency[i][j] = 0;
+                adjacency[i][j][0] = 0;
+    }
+
+    Graph construct_mst()
+    {
+        for (int i = 0; i < n; i++)
+            visited[i] = 0;
+
+        priority_queue<Edge> pq;
+        for (int i = 0; i < n; i++)
+            if (adjacency[0][i][0])
+                pq.push(Edge(0, i, adjacency[0][i][1]));
+
+        Graph mst(n);
+        int edges_remaining = n - 1;
+        visited[0] = 1;
+
+        while (edges_remaining)
+        {
+            Edge edge = pq.top();
+            pq.pop();
+            if (visited[edge.dest])
+                continue;
+
+            mst.add_edge(edge.src, edge.dest, edge.weight);
+            edges_remaining--;
+
+            for (int i = 0; i < n; i++)
+                if (adjacency[edge.dest][i][0])
+                {
+                    pq.push(Edge(edge.dest, i, adjacency[edge.dest][i][1]));
+                }
+
+            visited[edge.dest] = 1;
+        }
+
+        return mst;
     }
 
     void destroy_graph()
@@ -179,6 +236,8 @@ public:
 
         for (int i = 0; i < n; i++)
         {
+            for (int j = 0; j < n; j++)
+                delete[] adjacency[i][j];
             delete[] adjacency[i];
         }
 
@@ -189,24 +248,25 @@ public:
 
 int main(void)
 {
-    Graph graph(6);
+    Graph graph(5);
 
-    graph.add_edge(1, 3);
-    graph.add_edge(1, 2);
-    graph.add_edge(1, 4);
-    graph.add_edge(4, 5);
-    graph.add_edge(3, 5);
-    graph.add_edge(2, 3);
+    graph.add_edge(0, 2, 5);
+    graph.add_edge(0, 1, 10);
+    graph.add_edge(0, 3, 20);
+    graph.add_edge(3, 4, 15);
+    graph.add_edge(2, 4, 5);
+    graph.add_edge(1, 2, 10);
 
     cout << "adjacency matrix:" << endl;
     graph.display();
 
-    /*
+    /*           20
            1----------4
            | \         \
-           |  \         \
+        10 |  \ 5       \ 15
            |   \         \
            2----3---------5
+             10       5
     */
 
     cout << "dfs: ";
@@ -215,5 +275,9 @@ int main(void)
     cout << "bfs: ";
     graph.bfs();
 
+    Graph mst = graph.construct_mst();
+    mst.display();
+
+    mst.destroy_graph();
     graph.destroy_graph();
 }
